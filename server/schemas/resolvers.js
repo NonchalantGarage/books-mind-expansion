1f20fc2104f0304ac4bbc05a105433d12a4d063c
curtis <hong.curtis@gmail.com>
2022-03-30 22:03:43 -0700

    add query and mutation in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,25 @@
 const { User } = require("../models");
 
+// create signToken with JWT
+const {signToken}= require('../utils/auth')
+
+
+
 const resolvers = {
+    Query: {
+        user: async (parent, {_id}) =>{
+            return User.findOne({_id});
+        
+        }
+    },
+    Mutation: {
+        createUser: async (parent, args) =>{
+            const user = User.create(args);
+            const token = signToken(user);
+
+            return {token, user};
+        }
+    }
     
 }
 
